907d8df6e3f493429751de92643f29feb1c31593
reese8272 <reesepludwick@gmail.com>
2025-02-28 15:31:19 -0500

    bug fixes. Make sure you have python 3.11 lol

--- src/model.py
+++ src/model.py
@@ -4,9 +4,10 @@ def create_model(vocab_size=10000, max_length=100):
     """Creates and returns a text classification model."""
     
     model = tf.keras.Sequential([
-        tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=128),
-        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Embedding(input_dim=vocab_size, output_dim=64),
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(32)),
         tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dropout(0.4),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
